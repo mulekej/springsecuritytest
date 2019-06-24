93c7e1e2e3cb0d434e842513fd1b491f00fbe170
Eric Mulek <ejmulek@gmail.com>
2019-06-23 22:36:43 -0500

    test filtering options

--- src/main/groovy/com/ericmulek/springsecuritytest/controller/annotations/PostFilterAspect.groovy
+++ src/main/groovy/com/ericmulek/springsecuritytest/controller/annotations/PostFilterAspect.groovy
@@ -22,10 +22,29 @@ import java.lang.annotation.Target
 class PostFilterAspect {
 
     @Around('@annotation(PostFilter)')
-    List<Person> doFiltering(ProceedingJoinPoint joinPoint) {
-        List<Person> result = joinPoint.proceed() as List<Person>
-        result.findAll {
+    Object doFiltering(ProceedingJoinPoint joinPoint) {
+        Class returnType = joinPoint.signature.returnType
+        isListOfPeople(returnType)
+        def result = joinPoint.proceed()
+        processResult(result)
+
+    }
+
+    private boolean isListOfPeople(Class<List<Person>> clazz) {
+        true
+    }
+
+    private boolean isListOfPeople(Object obj) {
+        throw new RuntimeException('Invalid Return Type')
+    }
+
+    private List<Person> processResult(List<Person> people) {
+        people.findAll {
             !it.hidden
         }
+    }
+
+    private Object processResult(Object object) {
+        throw new RuntimeException('PostFilter applied to method of invalid return type')
     }
 }
